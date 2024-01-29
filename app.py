2e2fb30d3dd5469336abb1c5040cb93639582c17
sksarvesh007 <bt22csd002@iiitn.ac.in>
2024-01-29 22:59:39 +0530

    changes in login and register page

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY']='thisissecretkey'
+db = SQLAlchemy(app)
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
